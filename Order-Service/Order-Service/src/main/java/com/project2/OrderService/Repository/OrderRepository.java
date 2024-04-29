fb3d92a560acd00bb5a43c88b29bb8fa9a8b76bf
Mariam Gamal <Mariaamgamaal25@gmail.com>
2024-04-29 18:24:16 +0300

    practice

--- Order-Service/Order-Service/src/main/java/com/project2/OrderService/Repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project2.OrderService.Repository;
-
-import com.project2.OrderService.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- Product-service/Product-service/src/main/java/com/Project1/Productservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Project1.Productservice.repository;
-
-import com.Project1.Productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
